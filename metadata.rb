7acf5fa209285c45594c7b16f7d76b837aea8e90
Josh Blancett <joshblancett@gmail.com>
2014-06-17 02:33:24 -0400

    initial metadata

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'hipache'
+maintainer       'Firma 8'
+maintainer_email 'Josh@Firma8.com'
+license          'All rights reserved'
+description      'Installs/Configures hipache on Ubuntu'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+depends          'nodejs'
