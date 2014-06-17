a6782e42e10861e8777a4bce6383cf684c347108
Josh Blancett <joshblancett@gmail.com>
2014-06-17 04:25:37 -0400

    fixing template

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'Josh@Firma8.com'
 license          'All rights reserved'
 description      'Installs/Configures hipache on Ubuntu'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 depends          'nodejs'
